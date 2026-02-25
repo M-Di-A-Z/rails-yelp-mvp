d0159d418e7a626cbad805704fe67c1513374cdd
Marina Diaz <m.diaz@mailistec.fr>
2026-02-25 19:34:23 +0100

    resto

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,9 +4,26 @@ class RestaurantsController < ApplicationController
   end
   def show
     @restaurant = Restaurant.find(params[:id])
+    @restaurant
+
   end
 
   def new
     @restaurant = Restaurant.new()
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "restaurant", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
+  get "restaurant/index/id", to: "restaurants#index"
+  get "restaurants/new", to: "new_restaurant_path#new"
   get "restaurants/id", to: "restaurants#show"
+  post "/restaurants", to: "restaurants#create"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
